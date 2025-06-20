70a77e541d0b188ad3d235d018a913ff3edd8e20
rithwik_pro <pr.rithwik.pro>
2025-06-20 11:56:15 +0000

    Add document structure

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 PROJECT_ROOT = "/content/drive/MyDrive/Projects/GitHub/ClusterNetflix"
 DATA_PATH = f"{PROJECT_ROOT}/data"
 NOTEBOOKS_PATH = f"{PROJECT_ROOT}/notebooks"
+DOCS_PATH = f"{PROJECT_ROOT}/docs"
